66da82d67f89d6fd8c4e24f7a7a8a2f64691838b
yangarbiter <yangarbiter@gmail.com>
2017-12-09 06:55:57 +0800

    added RAKEL

--- mlearn/models/__init__.py
+++ mlearn/models/__init__.py
@@ -3,3 +3,4 @@
 #from .rakel import RAKEL
 from .classifier_chains import ClassifierChains
 from .probabilistic_classifier_chains import ProbabilisticClassifierChains
+from .random_k_labelsets import RandomKLabelsets
